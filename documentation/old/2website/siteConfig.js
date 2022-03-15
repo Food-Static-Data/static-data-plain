f60e7d38b1b5a7eac07d0701b7b7197117b26c14
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-03-15 23:45:19 +0200

    Update siteConfig.js

--- documentation/old/2website/siteConfig.js
+++ documentation/old/2website/siteConfig.js
@@ -1,24 +1,4 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 
-// See https://docusaurus.io/docs/site-config.html for all the possible
-// site configuration options.
-
-/* List of projects/orgs using your project for the users page */
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
 
 const siteConfig = {
   title: 'GroceriStar Fetch Module' /* title for your website */,
@@ -58,17 +38,6 @@ const siteConfig = {
     secondaryColor: '#205C3B',
   },
 
-  /* custom fonts for website */
-  /*fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },*/
 
   // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
   copyright:
@@ -76,24 +45,10 @@ const siteConfig = {
     new Date().getFullYear() +
     ' GroceriStar company',
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks
-    theme: 'default',
-  },
-
-  // Add custom scripts here that would be placed in <script> tags
-  scripts: ['https://buttons.github.io/buttons.js'],
-
-  /* On page navigation for the current documentation page */
-  onPageNav: 'separate',
-
-  /* Open Graph and Twitter card images */
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+ 
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
     repoUrl: 'https://github.com/GroceriStar/groceristar-fetch',
 };
 
-module.exports = siteConfig;
